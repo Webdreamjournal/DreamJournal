52260341fae2849b0de200ee7211685382546927
Webdreamjournal <webdreamjournal@gmail.com>
2025-09-15 14:21:42 +1000

    vv2.04.01 2025-15-09_14-21-41 - EncryptionBugfixing

--- sw.js
+++ sw.js
@@ -56,7 +56,7 @@
  * @constant {string}
  * @since 2.0.0
  */
-const CACHE_NAME = 'dream-journal-v2-04-00';
+const CACHE_NAME = 'dream-journal-v2-04-01';
 
 /**
  * List of essential files to cache for complete offline functionality.
